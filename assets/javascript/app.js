1d0627963870ea4e0fb8c29639370b300876d66d
VishnuSwaminathan <vishcodes@gmail.com>
2018-07-20 05:29:54 -0400

    thirdCommit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,31 +21,50 @@ $('button').on('click', function() {
       var p = $('<p>').text('Rating: ' + results[i].rating);
       var videogameImage = $('<img>');
       videogameImage.attr('src', results[i].images.fixed_height_still.url);
+
       videogameDiv.append(p);
       videogameDiv.append(videogameImage);
       $('#gifs-appear-here').prepend(videogameDiv);
     }
+    $('img').on('click', function() {
+      var gifClicked = this;
+      console.log('Gif Clicked: ' + gifClicked);
+
+      var state = $(this).attr('src');
+      var still = results.images.fixed_height_still.url;
+      var animate = results.images.fixed_heigh.url;
+      console.log('State of GIF: ' + state);
+
+      if (state == still) {
+        this.attr('src', results.images.fixed_height.url);
+      } else if (state == animate) {
+        this.attr('src', results.images.fixed_height_still.url);
+      }
+    });
   });
 });
-$('#gifs-appear-here').on('click', function() {
-  console.log('clicked gif');
-  var queryURL =
-    'https://api.giphy.com/v1/gifs/search?q=' +
-    videogame +
-    '&api_key=k57hF1C8XQZEgLOoyBIn2TgzKU72Gdzr&limit=10';
-  $.ajax({
-    url: queryURL,
-    method: 'GET'
-  }).then(function(response) {
-    var results = response.data;
-    var still = results.images.fixed_height_still.url;
-    var animate = results.images.fixed_heigh.url;
-    var state = $(this).attr('src');
-    console.log('State of GIF: ' + state);
-    if (state == still) {
-      this.attr('src', results.images.fixed_height.url);
-    } else if (state == animate) {
-      this.attr('src', results.images.fixed_height_still.url);
-    }
-  });
-});
+
+// $('#gifs-appear-here').on('click', function() {
+//   console.log('clicked gif');
+//   var queryURL =
+//     'https://api.giphy.com/v1/gifs/search?q=' +
+//     videogame +
+//     '&api_key=k57hF1C8XQZEgLOoyBIn2TgzKU72Gdzr&limit=10';
+//   $.ajax({
+//     url: queryURL,
+//     method: 'GET'
+//   }).then(function(response) {
+//     var results = response.data;
+
+//     var still = results.images.fixed_height_still.url;
+//     var animate = results.images.fixed_heigh.url;
+//     var state = $(this).attr('src');
+//     console.log('State of GIF: ' + state);
+
+//     if (state == still) {
+//       this.attr('src', results.images.fixed_height.url);
+//     } else if (state == animate) {
+//       this.attr('src', results.images.fixed_height_still.url);
+//     }
+//   });
+// });
